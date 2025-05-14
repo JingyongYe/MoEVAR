d50916de4b7e6c790e286e2734e695531667e6f5
Jingyong Ye <jyye21@m.fudan.edu.cn>
2025-05-14 20:25:03 +0800

    enhanced moevar train fast

--- training/moevar_d16_15_4top2_256.sh
+++ training/moevar_d16_15_4top2_256.sh
@@ -1,5 +1,5 @@
 torchrun --nproc_per_node=4 --nnodes=1 --node_rank=0 --master_addr="localhost" --master_port=29500 /private/yjy/project/VAR/moetrain.py \
-  --depth=16 --bs=1024 --pn=256 --ep=200 --fp16=1 --alng=1e-3 --wpe=0.1 \
+  --depth=16 --bs=1024 --pn=256 --ep=200 --fp16=1 --alng=1e-3 --wpe=0.1 --workers=16 \
   --data_path="/private/yjy/project/VAR/datasets/imagenet_organized" \
   --local_out_dir_path="/private/yjy/project/VAR/output/moevar_d16_15_4top2_256" \
   --var_ckpt_path="/private/yjy/project/VAR/training/var_d16.pth" \
